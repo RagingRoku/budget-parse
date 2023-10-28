4147b2569af7cef44b8f1e065bcfa49cd64d583d
Martin Rochin <mrochin@Martins-MBP.lan>
2023-10-28 14:34:23 -0700

    feat(main): first commit

---
+++ budget-parse/src/app.ts
@@ -0,0 +1,108 @@
+import express from 'express';
+import * as path from 'path';
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
+
+const app = express();
+const port = 3000;
+const csvFilePath = path.resolve(__dirname, '../src/data/transactions.csv');
+const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+
+const headers = [
+  'transactionDate',
+  'postedDate',
+  'CardNumber',
+  'description',
+  'category',
+  'debit',
+  'credit'
+];
+
+type Transaction = {
+  transactionDate: string,
+  postedDate: string,
+  CardNumber: string,
+  description: string,
+  category: string,
+  debit: string,
+  credit: string
+};
+
+type CleanTransaction = {
+  transactionDate: string,
+  descriptions: string[],
+  debits: string[],
+};
+
+type SheetsRow = {
+  transactionDate: string,
+  descriptionString: string,
+  debitString: string
+}
+
+const transformCSV = (originalInput: Transaction[]) => {
+  const newCSV:CleanTransaction[] = [];
+  // reverse original array to get transactions from new to old
+  originalInput.reverse();
+  // ignore last row (the headers)
+  originalInput.slice(0, -1).map((original) => {
+    // ignore row if no debit
+    if (!original.debit){
+      console.log('no debit');
+      return;
+    }
+    const transactionInNewCSV = newCSV.some((row) => row.transactionDate.includes(original.transactionDate))
+    // row doesn't exist, map the whole object
+    if (!transactionInNewCSV) {
+      newCSV.push({
+        transactionDate: original.transactionDate,
+        descriptions: [original.description],
+        debits: [original.debit]
+      })
+    }
+    // row exists, add description and debit
+    if (transactionInNewCSV) {
+      const matchingIndex = newCSV.findIndex((row) => row.transactionDate == original.transactionDate)
+      newCSV[matchingIndex].descriptions.push(original.description)
+      newCSV[matchingIndex].debits.push(original.debit)
+    }
+  });
+  return newCSV;
+}
+
+const prettyForSheets = (cleanTransactions: CleanTransaction[]) => {
+  const sheetsData: SheetsRow[] = [] 
+  cleanTransactions.map((row) => {
+    sheetsData.push({
+      transactionDate: row.transactionDate,
+      descriptionString: row.descriptions.join(', '),
+      debitString: `= sum ( ${row.debits.join(' + ')} )`
+    });
+  });
+  return sheetsData;
+};
+
+app.get('/', (req, res) => {
+  parse(fileContent, {
+    delimiter: ',',
+    columns: headers,
+  }, (error, result: Transaction[]) => {
+    // console.log("Result", result);
+    console.log("Result type", typeof result);
+    const newResult = transformCSV(result);
+    console.log("newResult", newResult);
+    console.log("newResult type", typeof newResult);
+
+    const dataForSheets = prettyForSheets(newResult);
+    console.log("dataForSheets", dataForSheets);
+    console.log("dataForSheets type", typeof dataForSheets);
+
+
+  });
+  res.send(`Hello World!`);
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
